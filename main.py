d3a6e1313d45e7ef394f17f83b83402b5bdfe999
anna-sb18 <acdeoliveira101@gmail.com>
2024-05-20 01:35:58 -0300

    Testando commit and push integrados no VSCode.

--- main.py
+++ main.py
@@ -5,3 +5,20 @@
 # Sort
 # Append
 # Insert
+
+# import math
+# import time
+# time.sleep(n)  n = seg
+
+# pass - deixa o fluxo continuar
+# break - interrompe o fluxo do while
+# continue - interrompe apenas a iteração atual do while
+
+#for !!
+#  iteratior ou generator
+
+# print() pula uma linha
+
+# f''  formata uma string, para colocar comandos dentro usar {}. Ex:
+# ex = 'Anna'
+# print(f'Oi, {ex}.')
